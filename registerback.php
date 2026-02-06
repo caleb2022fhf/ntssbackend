d3b0e097a8a72da6474a0e33e2e70fda7b3380f0
caleb2022fhf <aimecaleb05@gmail.com>
2026-02-06 19:58:09 +0200

    Add files via upload

---
+++ registerback.php
@@ -0,0 +1,21 @@
+{
+  "name": "nyanza-tss-app",
+  "version": "1.0.0",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js",
+    "dev": "nodemon server.js"
+  },
+  "dependencies": {
+    "bcrypt": "^5.1.0",
+    "express": "^4.18.2",
+    "helmet": "^7.0.0",
+    "sqlite3": "^5.1.6",
+    "express-rate-limit": "^7.0.0",
+    "cors": "^2.8.5",
+    "body-parser": "^1.20.2"
+  },
+  "devDependencies": {
+    "nodemon": "^2.0.22"
+  }
+}
